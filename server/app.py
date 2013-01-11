c4a6bd7a45ef61bebfdb8b3e1df662062edf73b3
okamurayasuyuki <haradashinya@gmail.com>
2013-01-12 00:21:14 +0900

    add BeautifulSoup

--- server/app.py
+++ server/app.py
@@ -1,9 +1,17 @@
+import requests
 from flask import Flask
 from flask import jsonify
+from BeautifulSoup import *
 import json
 
 
 app = Flask(__name__)
+
+def news_links(dom_content):
+    soup = BeautifulSoup(dom_content)
+    links = soup.findAll("a")
+    return links
+    
 
 @app.route("/")
 def hello():
@@ -11,6 +19,13 @@ def hello():
 
 @app.route("/news")
 def news():
+    r = requests.get("http://javascriptweekly.com/archive/1.html")
+    # show content
+    body = r.content
+    links = news_links(body)
+    print links
+    print r.url
+    print r.status_code
     return jsonify(data=["apple","orange","banana"])
 
 
