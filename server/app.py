c21f2208613303da1f24534388d6ba193a53d9bf
okamurayasuyuki <haradashinya@gmail.com>
2013-01-21 00:37:45 +0900

    return all news order by newest.

--- server/app.py
+++ server/app.py
@@ -2,15 +2,54 @@ import requests
 from flask import Flask
 from flask import jsonify
 from BeautifulSoup import *
+from models.link import Link
+
 import json
+import re
 
 
 app = Flask(__name__)
+link = Link()
+
+def is_valid_link(link):
+    match = re.match(r'.*\.html',str(link))
+    if match:
+        return True
+    return False
 
 def news_links(dom_content):
     soup = BeautifulSoup(dom_content)
     links = soup.findAll("a")
-    return links
+    res = []
+    for link in links:
+        if is_valid_link(link):
+            res.append(link)
+    return sorted_links(links)
+
+## append links to link.objects
+def inject_links(links):
+    for l in links:
+        tmp  =  re.split(r'<a\shref=',str(l))
+        number =  re.sub(r'\.*html','',tmp[1]).split(">")[0]
+        if re.match(r'"\/"',str(number)):
+            pass
+        else:
+            link.objects.append(
+                    {"number": int(number.strip('"')),
+                        "body": l}
+                    )
+
+## set link.objects order by newest.
+def sorted_links(links):
+    inject_links(links)
+    link.objects = sorted(link.objects,key = lambda link_obj:link_obj["number"])
+    return link.objects
+
+def latest_link():
+    latest = link.objects.pop()
+    return latest
+
+        
     
 
 @app.route("/")
@@ -19,13 +58,10 @@ def hello():
 
 @app.route("/news")
 def news():
-    r = requests.get("http://javascriptweekly.com/archive/1.html")
-    # show content
+    r = requests.get("http://javascriptweekly.com/archive/")
+    print r.status_code
     body = r.content
     links = news_links(body)
-    print links
-    print r.url
-    print r.status_code
     return jsonify(data=["apple","orange","banana"])
 
 
