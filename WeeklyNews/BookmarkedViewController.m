81b9696ec7e192ba46b510fb85710c0e68bac9c5
okamurayasuyuki <haradashinya@gmail.com>
2013-02-06 23:10:51 +0900

    sort by latest bookmarked.

--- WeeklyNews/BookmarkedViewController.m
+++ WeeklyNews/BookmarkedViewController.m
@@ -32,7 +32,11 @@
     
     ud = [NSUserDefaults standardUserDefaults];
     
-    bookmarkedArray = [ud objectForKey:@"bookmarkedArray"];
+//    NSArray* reversedArray = [[startArray reverseObjectEnumerator] allObjects];
+
+    bookmarkedArray = [[[ud objectForKey:@"bookmarkedArray"] reverseObjectEnumerator] allObjects] ;
+//    NSArray* reversedArray = [[startArray reverseObjectEnumerator] allObjects];
+
     NSLog(@"bookmark e is %@",bookmarkedArray);
     
 }
@@ -90,44 +94,8 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
 
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
 
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
